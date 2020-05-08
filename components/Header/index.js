f55187944d4b0196d765d7a2215608e813773c4a
lateralcreativity <lateralcreativity@outlook.com>
2020-05-08 10:19:05 -0500

    Basic header component set up

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,6 +11,33 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
+const create = (element) => document.createElement(element);
+
+
 function Header() {
-    
+    // Create Elements
+    const header = create('div');
+    const date = create('span');
+    const title = create('h1');
+    const temp = create('span');
+
+    // Assign Classes
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    // Set content
+    date.textContent = 'MARCH 28, 2019';
+    title.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    // Append stuff
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+
+    // Good to go
+    return header;
 }
+
+// console.log(Header()); // Check if working
